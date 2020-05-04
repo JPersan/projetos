51dd9c29f3e63033e1ada2cc9c7091788714f3b3
Jeferson <jefersonpsantos@gmail.com>
2020-05-04 18:51:06 -0300

    inclusao de projeto

---
+++ Report/googlesheet.rb
@@ -0,0 +1,10 @@
+ Volume in drive C has no label.
+ Volume Serial Number is F861-6580
+
+ Directory of C:\Users\je.santos\Desktop\projetos\Report
+
+05/04/2020  06:49 PM    <DIR>          .
+05/04/2020  06:49 PM    <DIR>          ..
+05/04/2020  06:49 PM                 0 googlesheet.rb
+               1 File(s)              0 bytes
+               2 Dir(s)  122,112,434,176 bytes free
