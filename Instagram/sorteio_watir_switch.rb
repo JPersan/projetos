5e262c0936edcea09e466df09f1232f9d357a448
je.santos <je.santos@reply.com>
2021-01-27 13:11:27 -0300

    alt de conta

--- Instagram/sorteio_watir_switch.rb
+++ Instagram/sorteio_watir_switch.rb
@@ -2,10 +2,9 @@ require 'watir'
 require 'gemoji'
 require_relative 'emoji.rb'
 
-class Instragram
+class Instagram
 
-    
-    def comment
+    def start
         puts '=> Você deseja visualizar a execução?
                 1 - Sim
                 2 - Não'
@@ -17,66 +16,68 @@ class Instragram
             headless = true
         end
 
-        # puts '=> Informe seu usuário:'
-        # user = gets.chomp.to_s
-
-        # puts '=> Informe sua senha:'
-        # pass = gets.chomp.to_s
-
-        # puts '=> Informe a quantidade de comentários que deseja fazer:'
-        # qtde = gets.chomp.to_i
-
-        # puts '=> Escreva seu comentário:'
-        # msg = gets.chomp.to_s
-
-        user = 'jpersan01'
-        pass = '02042010'
-        qtde = 2
+        @qtde = 2
 
         Watir.logger.level = :error
-        browser = Watir::Browser.new :chrome, headless: headless
-        browser.goto "https://www.instagram.com"
+        @browser = Watir::Browser.new :chrome, headless: headless
+        @browser.goto "https://www.instagram.com"
+        @user = 1
+        login
+        @browser.send_keys :enter
+        conta
+        #comment
+    end
 
-        browser.text_field(name: 'username').set user
-        browser.text_field(name: 'password').set pass
-
-        browser.send_keys :enter
-        sleep 5
-
-
-        browser.text_field(class_name: 'XTCLo').set 'binhhoovideos'
-        browser.div(class: 'uyeeR').click
-
-        x = 0 
-        while x < qtde       
-            browser.div(class: 'v1Nh3 kIKUG  _bz0w', index: 0).click
-            browser.div(class: 'RxpZH').click
-            browser.textarea(class: 'Ypffh').set $emoji.raw
-            browser.send_keys :enter
-            sleep 5
-
-            if x == 2 
-                browser.send_keys :escape
-                conta
-            else
-                browser.send_keys :escape
-                puts x
-                sleep 15
-            end
-            x+=1
-
-        end
+    def login
+        @browser.text_field(name: 'username').set "jpersan0#{@user}"
+        @browser.text_field(name: 'password').set '02042010'
     end
 
     def conta
-        browser.div(class: 'Fifk5', index: 4).click #click perfil
-        browser.div(id: 'fb7c9e0ed08fc4').click #click opção switch
-        #browser.text.include? 'jpersan02'
-        browser.button(text: 'Log into an Existing Account').click #click botão para logar nova conta
-    end
+        @browser.div(class: 'Fifk5', index: 4).click
+        @browser.div(class: '-qQT3').click
+        @user = 2
 
+        if @browser.div(text: 'jpersan01' && 'jpersan02') == false
+               @browser.button(text: 'Log into an Existing Account').click #click botão para logar nova conta
+               login
+               @browser.div(class: 'mwD2G').click
+               @browser.button(text: 'Log In').click
+
+               if @user == 2
+                    @user = 1
+               end
+        end
+    end
 end
 
+    # def comment
+    #     @browser.text_field(class_name: 'XTCLo').set 'binhhoovideos'
+    #     @browser.div(class: 'uyeeR').click
 
-sorteio = Instragram.new
-sorteio.comment
+    #     x = 0 
+    #     while x < @qtde       
+    #         @browser.div(class: 'v1Nh3 kIKUG  _bz0w', index: 1).click
+    #         @browser.div(class: 'RxpZH').click
+    #         @browser.textarea(class: 'Ypffh').set $emoji.raw
+    #         @browser.send_keys :enter
+    #         sleep 5
+
+    #         puts x
+    #         if x == 1 
+    #             @browser.send_keys :escape
+    #             switch_accounts
+    #         else
+    #             @browser.send_keys :escape
+    #             puts x
+    #             sleep 15
+    #         end
+    #         x+=1
+
+    #     end
+    # end
+#end
+
+
+sorteio = Instagram.new
+sorteio.start
