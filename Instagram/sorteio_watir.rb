7af57254806e412b729172d67eaf6ef5233486f1
je.santos <je.santos@reply.com>
2021-01-27 13:10:48 -0300

    alt proj

--- Instagram/sorteio_watir.rb
+++ Instagram/sorteio_watir.rb
@@ -13,17 +13,22 @@ else
     headless = true
 end
 
-puts '=> Informe seu usuário:'
-user = gets.chomp.to_s
+# puts '=> Informe seu usuário:'
+# user = gets.chomp.to_s
 
-puts '=> Informe sua senha:'
-pass = gets.chomp.to_s
+# puts '=> Informe sua senha:'
+# pass = gets.chomp.to_s
 
-puts '=> Informe a quantidade de comentários que deseja fazer:'
-qtde = gets.chomp.to_i
+# puts '=> Informe a quantidade de comentários que deseja fazer:'
+# qtde = gets.chomp.to_i
 
-puts '=> Escreva seu comentário:'
-msg = gets.chomp.to_s
+# puts '=> Escreva seu comentário:'
+# msg = gets.chomp.to_s
+
+user = 'jpersan01'
+pass = '02042010'
+qtde = 1
+
 
 Watir.logger.level = :error
 browser = Watir::Browser.new :chrome, headless: headless
@@ -33,12 +38,15 @@ browser.text_field(name: 'username').set user
 browser.text_field(name: 'password').set pass
 
 browser.send_keys :enter
+sleep 5
+
+
 browser.text_field(class_name: 'XTCLo').set 'binhhoovideos'
 browser.div(class: 'uyeeR').click
 
 x = 0 
 while x < qtde       
-    browser.div(class: 'v1Nh3 kIKUG  _bz0w', index: 0).click
+    browser.div(class: 'v1Nh3 kIKUG  _bz0w', index: 1).click
     browser.div(class: 'RxpZH').click
     browser.textarea(class: 'Ypffh').set $emoji.raw
     browser.send_keys :enter
