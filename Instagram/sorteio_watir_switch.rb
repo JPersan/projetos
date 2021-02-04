e45fb10fb602a493b27c251e148476740c41e155
je.santos <je.santos@reply.com>
2021-02-04 10:18:38 -0300

    alt nome arq

--- Instagram/sorteio_watir_switch.rb
+++ Instagram/sorteio_watir_switch.rb
@@ -4,43 +4,74 @@ require_relative 'emoji.rb'
 
 class Instagram
 
-    def initialize(user, search)
-        puts '=> Você deseja visualizar a execução?
-                1 - Sim
-                2 - Não'
-        headless = gets.chomp.to_i
+    def initialize(search)
+        # puts '=> Você deseja visualizar a execução?
+        #         1 - Sim
+        #         2 - Não'
+        # headless = gets.chomp.to_i
 
-        if headless == 1
-            headless = false
-        else
-            headless = true
-        end
-        @user = user
+        # if headless == 1
+        #     headless = false
+        # else
+        #     headless = true
+        # end
+
         @pesquisa = search
         @qtde = 10000
+        @usuarios = ["jpersan01", "jpersan02", "jpersan03", "jpersan04", "jpersan05", "jpersan06"]
+        #@usuarios = ["jpersan01", "jpersan02", "jpersan04"]
+        @user = 0
+
+        # puts '=> Quais usuários voce pretende logar no instagram? (Para cada usuário digite e pressione Enter.)'
+        
+        # while true
+        #     input = gets.chomp
+        #     break if input.empty?
+        #         @usuarios << input
+        # end
+        
+        # @usuarios.join(", ")
 
         Watir.logger.level = :error
-        @browser = Watir::Browser.new :chrome, headless: headless
+        @browser = Watir::Browser.new :chrome, headless: false
         @browser.goto "https://www.instagram.com"
+
         login
         @browser.send_keys :enter
         comments
     end
 
     def login
-        @browser.text_field(name: 'username').set "jpersan0#{@user}"
+        @browser.text_field(name: 'username').set @usuarios[@user]
         @browser.text_field(name: 'password').set '02042010'
     end
 
     def switch_accounts
         @y = 0
-        
+
+        if @user >= @usuarios.count
+            @user=0
+        end
+
         @browser.div(xpath: "//section/nav/div[2]/div/div/div[3]/div/div[5]").click
         @browser.div(xpath: "//section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div[2]/div[2]/div[1]").click
         @browser.button(text: 'Log into an Existing Account').click #click botão para logar nova conta
         login
         #@browser.div(class: 'mwD2G').click
         @browser.button(text: 'Log In').click
+        @browser.button(text: 'Log In').wait_while(&:present?)
+
+        if @browser.text.include?('There was a problem logging you into Instagram.')
+            @usuarios.delete(@usuarios[@user])
+            puts 'msg erro login'
+            @user+=1
+            login
+        elsif  
+            @browser.text_field(name: 'username').present?
+            login
+        else
+        end
+
         @browser.div(class: 'EPjEi').wait_while(&:present?)
 
     end
@@ -52,46 +83,37 @@ class Instagram
 
     def comments
         search(@pesquisa)
-
-        x = 0
+        x = 791
         @y = 0
+
         while x < @qtde       
-            @browser.div(class: 'v1Nh3 kIKUG  _bz0w', index: 1).click
+            @browser.div(class: 'v1Nh3 kIKUG  _bz0w', index: 1).click # O index é a posição da foto na timeline iniciada em 0
             @browser.div(class: 'RxpZH').click
             @browser.textarea(class: 'Ypffh').set $emoji.raw #+ " #{x}"
             @browser.send_keys :enter
             sleep 5
+            @browser.send_keys :escape
 
-            if @y == 3 or @browser.text.include?('post comment.')
-            #if @browser.text.include?('post comment.')
-                @browser.send_keys :escape
-                case @user
-                    when 1
-                        @user = 2
-                     when 2
-                        @user = 3
-                    when 3
-                        @user = 4
-                    when 4
-                        @user = 5
-                    when 5
-                        @user = 6
-                    when 6
-                        @user = 1
-                end
+            if  @y == 3
+                @user+=1 
                 switch_accounts
                 search(@pesquisa)
-            sleep 60
-            
+            elsif 
+                @browser.text.include?('post comment.')
+                @usuarios.delete(@usuarios[@user])
+                puts @usuarios
+                @user = 0
+                switch_accounts
+                search(@pesquisa)
             else
                 @browser.send_keys :escape
                 sleep 30
+                @y+=1
             end
-            puts x
             x+=1
-            @y+=1
+            puts x
         end
     end
 end
 
-sorteio = Instagram.new(1, 'binhhoovideos')
+sorteio = Instagram.new('redbrandao')
